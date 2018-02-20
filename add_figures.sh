738e10b6ceef7ee43e35100a88e1225c9b4a9f84
robbobbinett <rrobinet@mit.edu>
2018-02-20 04:26:38 -0500

    Programmatically insert Haine's images.

---
+++ add_figures.sh
@@ -0,0 +1,3 @@
+rm -f temp.tex
+cp chap1v65.tex temp.tex
+python3 getHainePDFs.py
---
+++ getHainePDFs.py
@@ -0,0 +1,15 @@
+import os
+rootdir = os.getcwd()
+
+image_names = []
+for _, _, files in os.walk(rootdir+'/haine_images'):
+	for file in files:
+		if file[::-1][0:3][::-1] == 'pdf' and file[0:7] != 'Ellipse':
+			image_names.append(file)
+
+figtext1 = '\\begin{figure}[H]\n\\centering\n\\includegraphics{'
+figtext2 = '}\n\\caption{This is a nice caption.}\n\\end{figure}\n'
+
+with open('temp.tex', 'a') as f:
+	for file in image_names:
+		f.write(figtext1+file+figtext2)
