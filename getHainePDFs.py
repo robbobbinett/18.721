954119a49b98eaf0aa0958852948d334dadd702c
robbobbinett <rrobinet@mit.edu>
2018-02-20 06:25:41 -0500

    Keep that which was sent to Artin; fixed appearance of filenames when figures appear

--- getHainePDFs.py
+++ getHainePDFs.py
@@ -7,10 +7,10 @@ image_names = []
 for root, _, files in os.walk(rootdir+'/haine_images'):
 	for file in files:
 		if file[::-1][0:3][::-1] == 'pdf' and file[0:7] not in verboten:
-			image_names.append(root+'/'+file)
+			image_names.append(root+'/'+file[:-4])
 
-figtext1 = '\\begin{figure}[H]\n\\centering\n\\includegraphics{'
-figtext2 = '}\n\\caption{This is a nice caption.}\n\\end{figure}\n\n'
+figtext1 = '\\begin{figure}[H]\n\\centering\n\\includegraphics{"'
+figtext2 = '".pdf}\n\\caption{This is a nice caption.}\n\\end{figure}\n\n'
 
 with open('temp.tex', 'a') as f:
 	for file in image_names:
