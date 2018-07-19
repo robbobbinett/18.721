6a6f29312be5dbf51a3f99da99679efce025f4d5
Ryan A Robinett <rrobinet@mit.edu>
2018-07-19 06:28:11 -0400

    Labels, captions solved; figure-width default to 5.0cm

--- add_figures.sh
+++ add_figures.sh
@@ -1,4 +1,4 @@
 ./clean.sh
 rm -f temp.tex
 cp ch1edit.tex temp.tex
-python3 getHainePDFs.py
+python3 getPDFs.py
---
+++ getPDFs.py
@@ -0,0 +1,20 @@
+import os
+rootdir = os.getcwd()
+
+verboten = ['Ellipse', 'Plot ge']
+
+image_names = []
+for root, _, files in os.walk(rootdir+'/images'):
+	for file in files:
+		if file[::-1][0:3][::-1] == 'pdf' and file[0:7] not in verboten:
+			image_names.append(root+'/'+file[:-4])
+
+figtext1 = '\\begin{figure}[H]\n\\centering\n\\begin{examples}{}\n\\label{goober'
+figtext2 = '}\n\\Marginnote{goober'
+figtext3 = '}\n\\end{examples}\n\\includegraphics[width=5.0cm]{"'
+figtext4 = '".pdf} \\\\\nThis is a nice caption.\n\\end{figure}\n\n'
+
+with open('temp.tex', 'a') as f:
+	for j, file in enumerate(image_names):
+		f.write(figtext1+str(j)+figtext2+str(j)+figtext3+file+figtext4)
+	f.write('\\end{document}\n')
