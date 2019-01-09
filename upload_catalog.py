b9b2ec5a599870771ea5b230167188b0ce6af104
Ryan A Robinett <rrobinet@mit.edu>
2019-01-09 11:56:26 -0500

    Uploading barring an error

--- upload_catalog.py
+++ upload_catalog.py
@@ -1,10 +1,42 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+store = file.Storage('token.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('drive', 'v3', http=creds.authorize(Http()))
+
+# Call the Drive v3 API
+results = service.files().list(
+    pageSize=10, fields="nextPageToken, files(id, name)").execute()
+items = results.get('files', [])
+
+if not items:
+    print('No files found.')
+else:
+    print('Files:')
+    for item in items:
+        print(u'{0} ({1})'.format(item['name'], item['id']))
+
 file_metadata = {
     'name': 'Figure Catalog',
     'mimeType': 'application/vnd.google-apps.spreadsheet'
 }
+
+from googleapiclient.http import MediaFileUpload
 media = MediaFileUpload('figure_catalog.xls',
                         mimetype='application/vnd.ms-excel',
                         resumable=True)
-file = drive_service.files().create(body=file_metadata,
+file = service.files().create(body=file_metadata,
                                     media_body=media,
                                     fields='id').execute()
