9d395e9f637cfb7c3d2c9569f85e4f1e313f7273
Ryan A Robinett <rrobinet@mit.edu>
2019-01-08 21:06:03 -0500

    Shell calls working

--- get_pngs.py
+++ get_pngs.py
@@ -9,26 +9,24 @@ for root, _, files in os.walk(rootdir+'/images'):
 		if file[::-1][0:3][::-1] == 'pdf' and file[0:7] not in verboten:
 			image_names.append(root+'/'+file[:-4])
 
-for name in image_names:
-	try:
-		os.remove(name+'.png')
-	except FileNotFoundError:
-		print(name+'.png does not exist.')
-
-from subprocess import Popen
-bash_template = "convert -density 150 REPLACE.pdf -quality 90 REPLACE.png"
+#from subprocess import Popen
+from subprocess import call
+bash_template = "pdfinfo REPLACE.pdf > read_del.txt"
 for name in image_names:
 	bash_command = bash_template.replace('REPLACE', name)
-	process = Popen(bash_command.split())
-	process.communicate()
-	process.kill()
+	call(bash_command, shell=True)
+	with open("read_del.txt", 'r') as f:
+		text = f.read()
+		text_cut = text[text.find("Page size:"), text.find("\nFile size:")]
+		text_cut = text_cut.replace(' ', '')
+		print(text_cut)
 
-figtext1 = '\\begin{figure}[H]\n\\centering\n\\begin{examples}{}\n\\label{goober'
-figtext2 = '}\n\\Marginnote{goober'
-figtext3 = '}\n\\end{examples}\n\\includegraphics[width=5.0cm]{"'
-figtext4 = '".png} \\\\\nThis is a nice caption.\n\\end{figure}\n\n'
+#figtext1 = '\\begin{figure}[H]\n\\centering\n\\begin{examples}{}\n\\label{goober'
+#figtext2 = '}\n\\Marginnote{goober'
+#figtext3 = '}\n\\end{examples}\n\\includegraphics[width=5.0cm]{"'
+#figtext4 = '".pdf} \\\\\nThis is a nice caption.\n\\end{figure}\n\n'
 
-with open('temp.tex', 'a') as f:
-	for j, file in enumerate(image_names):
-		f.write(figtext1+str(j)+figtext2+str(j)+figtext3+file+figtext4)
-	f.write('\\end{document}\n')
+#with open('temp.tex', 'a') as f:
+#	for j, file in enumerate(image_names):
+#		f.write(figtext1+str(j)+figtext2+str(j)+figtext3+file+figtext4)
+#	f.write('\\end{document}\n')
